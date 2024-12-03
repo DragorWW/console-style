565f7913bfcb361843345e5d67aa64fcaa30de95
Sergey Andreev <DragorWW@gmail.com>
2024-12-03 15:05:04 +0700

    Version: 1.0.1 fix import

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
+  plugins: [
+    dts({
+      include: ["src/**/*.ts"],
+      exclude: ["src/**/*.test.ts"],
+      rollupTypes: true,
+    }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, "src/console$.ts"),
       name: "ConsoleStyle",
-      fileName: "console$",
-      formats: ["es", "umd"],
+      formats: ["es", "cjs"],
+      fileName: (format) => `console$.${format === "es" ? "js" : "cjs.js"}`,
     },
     rollupOptions: {
       output: {
@@ -15,6 +23,5 @@ export default defineConfig({
       },
     },
     sourcemap: true,
-    minify: "esbuild",
   },
 });
